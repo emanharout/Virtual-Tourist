fd547c97b90aa96cde7715ce820f64dd45448f4e
Eman <emmanuoel.h@gmail.com>
2016-07-22 08:02:03 -0700

    Refactor

--- Virtual Tourist/PhotoAlbumViewController.swift
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -5,61 +5,55 @@ import MapKit
 import CoreData
 
 class PhotoAlbumViewController: UIViewController {
-    
-    var pin: Pin!
-	var photoURLs: [NSURL]?
-    var fetchedResultsController: NSFetchedResultsController!
-    let stack = CoreDataStack.sharedInstance
-    var insertedItemsIndex: [NSIndexPath]!
-    var deletedItemsIndex: [NSIndexPath]!
 	
-
-    
-    @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        mapView.addAnnotation(pin)
-        mapView.region = makeRegionWithAnnotation(pin)
-        
-        let itemDimension = ((view.frame.size.width - 10.0)/3)
-        flowLayout.minimumLineSpacing = CGFloat(5.0)
-        flowLayout.minimumInteritemSpacing = CGFloat(5.0)
-        flowLayout.itemSize = CGSize(width: itemDimension, height: itemDimension)
-        
-        if fetchPhotos().isEmpty {
-            getPhotoURLs()
-        } else {
-            // Setup UI
-            print("fetch was not empty")
-        }
-    }
-
-    
-    
-    // TODO: Check if lack of sort desc causes issue w/ results
-    func fetchPhotos() -> [Photo] {
-        let fetchRequest = NSFetchRequest(entityName: "Photo")
-        fetchRequest.sortDescriptors = [NSSortDescriptor]()
-        let predicate = NSPredicate(format: "pin = %@", argumentArray: [pin])
-        fetchRequest.predicate = predicate
-        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: stack.context, sectionNameKeyPath: nil, cacheName: nil)
-        fetchedResultsController.delegate = self
-        
-        var results = [Photo]()
-        do {
-            try fetchedResultsController.performFetch()
-            results = fetchedResultsController.fetchedObjects as! [Photo]
-            print("RESULTS: \(results)")
-        } catch {
-            print("Error when performing fetch")
-        }
-        return results
-    }
+	var pin: Pin!
+	var photoURLs: [NSURL]?
+	var fetchedResultsController: NSFetchedResultsController!
+	let stack = CoreDataStack.sharedInstance
+	var insertedItemsIndex: [NSIndexPath]!
+	var deletedItemsIndex: [NSIndexPath]!
+	
+	
+	
+	@IBOutlet weak var mapView: MKMapView!
+	@IBOutlet weak var collectionView: UICollectionView!
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		mapView.addAnnotation(pin)
+		mapView.region = makeRegionWithAnnotation(pin)
+		
+		setupFlowLayout()
+		
+		if fetchPhotos().isEmpty {
+			getPhotoURLs()
+		} else {
+			// Setup UI
+			print("fetch was not empty")
+		}
+	}
+	
+	func fetchPhotos() -> [Photo] {
+		let fetchRequest = NSFetchRequest(entityName: "Photo")
+		fetchRequest.sortDescriptors = [NSSortDescriptor]()
+		let predicate = NSPredicate(format: "pin = %@", argumentArray: [pin])
+		fetchRequest.predicate = predicate
+		fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: stack.context, sectionNameKeyPath: nil, cacheName: nil)
+		fetchedResultsController.delegate = self
+		
+		var results = [Photo]()
+		do {
+			try fetchedResultsController.performFetch()
+			results = fetchedResultsController.fetchedObjects as! [Photo]
+			print("RESULTS: \(results)")
+		} catch {
+			print("Error when performing fetch")
+		}
+		return results
+	}
 	
 	func getPhotoURLs() {
 		FlickrClient.sharedInstance.getPhotoURLsWithLocation(pin.latitude, longitude: pin.longitude) { (result, error) in
@@ -68,36 +62,43 @@ class PhotoAlbumViewController: UIViewController {
 			} else if let result = result {
 				self.photoURLs = result
 				//TODO: Additional Setup
-				
+				// ABC TESTing
 			}
 		}
 	}
 	
-    func performOnMainThread(block: ()->Void) {
-        let mainQueue = dispatch_get_main_queue()
-        dispatch_async(mainQueue, block)
-    }
-    
-    func makeRegionWithAnnotation(annotation: MKAnnotation) -> MKCoordinateRegion {
-        let center = annotation.coordinate
-        let span = MKCoordinateSpanMake(0.002, 0.002)
-        let region = MKCoordinateRegion(center: center, span: span)
-        return region
-    }
+	func performOnMainThread(block: ()->Void) {
+		let mainQueue = dispatch_get_main_queue()
+		dispatch_async(mainQueue, block)
+	}
+	
+	func makeRegionWithAnnotation(annotation: MKAnnotation) -> MKCoordinateRegion {
+		let center = annotation.coordinate
+		let span = MKCoordinateSpanMake(0.002, 0.002)
+		let region = MKCoordinateRegion(center: center, span: span)
+		return region
+	}
+	
+	func setupFlowLayout() {
+		let itemDimension = ((view.frame.size.width - 10.0)/3)
+		flowLayout.minimumLineSpacing = CGFloat(5.0)
+		flowLayout.minimumInteritemSpacing = CGFloat(5.0)
+		flowLayout.itemSize = CGSize(width: itemDimension, height: itemDimension)
+	}
 }
 
 
 
 extension PhotoAlbumViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        
-        
-//        let photo = fetchedResultsController.objectAtIndexPath(indexPath) as! Photo
-//        let photoData = photo.imageData
-//        let image = UIImage(data: photoData!)
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PhotoCell", forIndexPath: indexPath) as! PhotoCell
-        cell.imageView.image = UIImage(named: "placeholder")
+	
+	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+		
+		
+		//        let photo = fetchedResultsController.objectAtIndexPath(indexPath) as! Photo
+		//        let photoData = photo.imageData
+		//        let image = UIImage(data: photoData!)
+		let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PhotoCell", forIndexPath: indexPath) as! PhotoCell
+		cell.imageView.image = UIImage(named: "placeholder")
 		if let url = photoURLs?[indexPath.row] {
 			FlickrClient.sharedInstance.downloadDataFromURL(url) { (result, error) in
 				guard let data = result else {
@@ -109,73 +110,68 @@ extension PhotoAlbumViewController: UICollectionViewDelegate, UICollectionViewDa
 				}
 			}
 		}
-//
-//        cell.imageView.image = image
-//        collectionView.reloadData()
-//        
-//        return cell
-        
-        return cell
-    }
-    
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print("collect view number items")
-        return 21
-    }
-    
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        
-        
-    }
-    
-    func collectionView(collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
-        
-    }
-    
+		
+		return cell
+	}
+	
+	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		print("collect view number items")
+		return 21
+	}
+	
+	func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+		
+		
+	}
+	
+	func collectionView(collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
+		
+	}
+	
 }
 
 
 
 extension PhotoAlbumViewController: NSFetchedResultsControllerDelegate {
-    
-//    func controllerWillChangeContent(controller: NSFetchedResultsController) {
-//        insertedItemsIndex = [NSIndexPath]()
-//        deletedItemsIndex = [NSIndexPath]()
-//    }
-//    
-//    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
-//        
-//        switch type {
-//        case .Insert:
-//            insertedItemsIndex.append(newIndexPath!)
-//        case .Delete:
-//            deletedItemsIndex.append(indexPath!)
-//        default:
-//            break
-//        }
-//    }
-//    
-//    func controllerDidChangeContent(controller: NSFetchedResultsController) {
-//        collectionView.insertItemsAtIndexPaths(insertedItemsIndex)
-//        collectionView.deleteItemsAtIndexPaths(deletedItemsIndex)
-//    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+	
+	//    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+	//        insertedItemsIndex = [NSIndexPath]()
+	//        deletedItemsIndex = [NSIndexPath]()
+	//    }
+	//
+	//    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+	//
+	//        switch type {
+	//        case .Insert:
+	//            insertedItemsIndex.append(newIndexPath!)
+	//        case .Delete:
+	//            deletedItemsIndex.append(indexPath!)
+	//        default:
+	//            break
+	//        }
+	//    }
+	//
+	//    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+	//        collectionView.insertItemsAtIndexPaths(insertedItemsIndex)
+	//        collectionView.deleteItemsAtIndexPaths(deletedItemsIndex)
+	//    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- Virtual Tourist/TravelMapViewController.swift
+++ Virtual Tourist/TravelMapViewController.swift
@@ -5,140 +5,140 @@ import CoreData
 import MapKit
 
 class TravelMapViewController: UIViewController {
-    
-    let stack = CoreDataStack.sharedInstance
-    var editMode = false
-    var mapPosition: MapPosition!
-    
-    @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var editView: UIView!
-    @IBOutlet weak var editLabel: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        mapView.delegate = self
-        setupMapviewGestureRecognizer()
-        fetchPins()
-        mapPosition = MapPosition(mapView: mapView, storedMapRegion: nil)
-        mapPosition.retrieveMapRegion()
-        
-        if mapPosition.storedMapRegion != nil && !mapPosition.mapPositionWasSet {
-            mapView.centerCoordinate = mapPosition.storedMapRegion!.center
-        }
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        // Mapview span/altitude values are automatically modified and snaps to other levels until view appears, must set region here
-        if !mapPosition.mapPositionWasSet {
-            mapPosition.setMapToLastPosition()
-        }
-    }
-    
-    @IBAction func editPressed(sender: UIBarButtonItem) {
-        toggleEditMode(sender)
-    }
-    
-    func toggleEditMode(button: UIBarButtonItem) {
-        editMode = !editMode
-        if editMode {
-            UIView.animateWithDuration(0.25){
-                self.editView.hidden = false
-                self.editLabel.alpha = 1.0
-            }
-            button.title = "Done"
-        } else {
-            UIView.animateWithDuration(0.25){
-                self.editView.hidden = true
-                self.editLabel.alpha = 0.0
-            }
-            button.title = "Edit"
-        }
-    }
-    
-    func fetchPins() {
-        let fetchRequest = NSFetchRequest(entityName: "Pin")
-        var pins: [Pin]
-        do {
-            let result = try stack.context.executeFetchRequest(fetchRequest) as? [Pin]
-            if let result = result {
-                pins = result
-                refreshPins(pins)
-            }
-        } catch {
-            print("Failed to fetch Pin objects")
-        }
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showPinPhotos" {
-            let pin = sender as! Pin
-            let destinationVC = segue.destinationViewController as! PhotoAlbumViewController
-            destinationVC.pin = pin
-        }
-    }
+	
+	let stack = CoreDataStack.sharedInstance
+	var editMode = false
+	var mapPosition: MapPosition!
+	
+	@IBOutlet weak var mapView: MKMapView!
+	@IBOutlet weak var editView: UIView!
+	@IBOutlet weak var editLabel: UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		mapView.delegate = self
+		setupMapviewGestureRecognizer()
+		fetchPins()
+		mapPosition = MapPosition(mapView: mapView, storedMapRegion: nil)
+		mapPosition.retrieveMapRegion()
+		
+		if mapPosition.storedMapRegion != nil && !mapPosition.mapPositionWasSet {
+			mapView.centerCoordinate = mapPosition.storedMapRegion!.center
+		}
+	}
+	
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		// Mapview span/altitude values are automatically modified and snaps to other levels until view appears, must set region here
+		if !mapPosition.mapPositionWasSet {
+			mapPosition.setMapToLastPosition()
+		}
+	}
+	
+	@IBAction func editPressed(sender: UIBarButtonItem) {
+		toggleEditMode(sender)
+	}
+	
+	func toggleEditMode(button: UIBarButtonItem) {
+		editMode = !editMode
+		if editMode {
+			UIView.animateWithDuration(0.25){
+				self.editView.hidden = false
+				self.editLabel.alpha = 1.0
+			}
+			button.title = "Done"
+		} else {
+			UIView.animateWithDuration(0.25){
+				self.editView.hidden = true
+				self.editLabel.alpha = 0.0
+			}
+			button.title = "Edit"
+		}
+	}
+	
+	func fetchPins() {
+		let fetchRequest = NSFetchRequest(entityName: "Pin")
+		var pins: [Pin]
+		do {
+			let result = try stack.context.executeFetchRequest(fetchRequest) as? [Pin]
+			if let result = result {
+				pins = result
+				refreshPins(pins)
+			}
+		} catch {
+			print("Failed to fetch Pin objects")
+		}
+	}
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if segue.identifier == "showPinPhotos" {
+			let pin = sender as! Pin
+			let destinationVC = segue.destinationViewController as! PhotoAlbumViewController
+			destinationVC.pin = pin
+		}
+	}
 }
 
 
 
 extension TravelMapViewController: MKMapViewDelegate {
-    
-    func addPin() {
-        if let gestureRecognizer = mapView.gestureRecognizers?[0] {
-            if gestureRecognizer.state == .Began && !editMode {
-                let longTapPoint = gestureRecognizer.locationInView(mapView)
-                let coordinate = mapView.convertPoint(longTapPoint, toCoordinateFromView: mapView)
-                
-                let annotation = Pin(latitude: coordinate.latitude, longitude: coordinate.longitude, context: stack.context)
-                
-                mapView.addAnnotation(annotation)
-            }
-        }
-    }
-    
-    func refreshPins(pins: [Pin]) {
-        let allAnnotations = mapView.annotations
-        mapView.removeAnnotations(allAnnotations)
-        mapView.addAnnotations(pins)
-    }
-    
-    func setupMapviewGestureRecognizer() {
-        let gestureRec = UILongPressGestureRecognizer(target: self, action: #selector(addPin))
-        mapView.addGestureRecognizer(gestureRec)
-    }
-    
-    
-    // MARK: Delegate Methods
-    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-        mapPosition.storeRegionValues()
-    }
-    
-    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
-        let annotation = view.annotation as! Pin
-        if editMode {
-            mapView.removeAnnotation(annotation)
-            stack.context.deleteObject(annotation)
-            stack.save()
-        } else {
-            performSegueWithIdentifier("showPinPhotos", sender: annotation)
-        }
-    }
-    
-    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        var annotView = mapView.dequeueReusableAnnotationViewWithIdentifier("pinView") as? MKPinAnnotationView
-        
-        if annotView == nil {
-            annotView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinView")
-            annotView?.animatesDrop = true
-            return annotView
-        }
-        annotView?.annotation = annotation
-        annotView?.animatesDrop = true
-        return annotView
-    }
+	
+	func addPin() {
+		if let gestureRecognizer = mapView.gestureRecognizers?[0] {
+			if gestureRecognizer.state == .Began && !editMode {
+				let longTapPoint = gestureRecognizer.locationInView(mapView)
+				let coordinate = mapView.convertPoint(longTapPoint, toCoordinateFromView: mapView)
+				
+				let annotation = Pin(latitude: coordinate.latitude, longitude: coordinate.longitude, context: stack.context)
+				
+				mapView.addAnnotation(annotation)
+			}
+		}
+	}
+	
+	func refreshPins(pins: [Pin]) {
+		let allAnnotations = mapView.annotations
+		mapView.removeAnnotations(allAnnotations)
+		mapView.addAnnotations(pins)
+	}
+	
+	func setupMapviewGestureRecognizer() {
+		let gestureRec = UILongPressGestureRecognizer(target: self, action: #selector(addPin))
+		mapView.addGestureRecognizer(gestureRec)
+	}
+	
+	
+	// MARK: Delegate Methods
+	func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+		mapPosition.storeRegionValues()
+	}
+	
+	func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+		let annotation = view.annotation as! Pin
+		if editMode {
+			mapView.removeAnnotation(annotation)
+			stack.context.deleteObject(annotation)
+			stack.save()
+		} else {
+			performSegueWithIdentifier("showPinPhotos", sender: annotation)
+		}
+	}
+	
+	func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+		
+		var annotView = mapView.dequeueReusableAnnotationViewWithIdentifier("pinView") as? MKPinAnnotationView
+		
+		if annotView == nil {
+			annotView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinView")
+			annotView?.animatesDrop = true
+			return annotView
+		}
+		annotView?.annotation = annotation
+		annotView?.animatesDrop = true
+		return annotView
+	}
 }
 
 
